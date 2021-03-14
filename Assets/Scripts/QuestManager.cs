6c152b154bde2085e6e7381265f137679191f2b0
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-14 19:11:29 +0900

    ボタンを押したらステージ進行

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public StageUIManager stageUI;
+
+    int currentStage = 0; //現在のステージ進行度
+
+    private void Start()
+    {
+        stageUI.UpdateUI(currentStage);
+    }
+
+    //Nextボタンが押されたら
+    public void OnNextButton()
+    {
+        currentStage++;
+        Debug.Log("進行度増加"+ currentStage);
+        //進行度をUIに反映
+        stageUI.UpdateUI(currentStage);
+    }
+}
---
+++ Assets/Scripts/StageUIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//stageUIを管理
+public class StageUIManager : MonoBehaviour
+{
+    public Text stageText;
+
+    public void UpdateUI(int currentStage)
+    {
+        stageText.text = string.Format("ステージ : {0}", currentStage+1);
+        //stageText.text = currentStage.ToString();
+    }
+}
