0fef27ed7fb7be26d6ccf1bf4e70c26d771beb87
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-14 22:00:44 +0900

    プレイヤーのステータスを管理するファイル生成

---
+++ Assets/Scripts/PlayerUIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    public Text hpText;
+    public Text atText;
+
+    public void UpdateUI()
+    {
+        hpText.text = string.Format("HP : {0}", 80);
+
+    }
+}
