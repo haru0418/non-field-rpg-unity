bcd7a083b4d22360d4209921ac7a4ecee37e2e1f
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-24 20:04:30 +0900

    文字の背景の設定

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSourceSE; //SEのスピーカー
+    public AudioClip audioClip; //　ならす素材
+
+    void Start()
+    {
+    }
+
+    public void PlaySE()
+    {
+        audioSourceSE.PlayOneShot(audioClip);
+
+    }
+
+}
