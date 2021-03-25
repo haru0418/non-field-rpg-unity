24e49b69d112fe7170c5ab9cb12257703b28e667
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-25 16:19:04 +0900

    シーン間でのフェードアウト

--- Assets/Scripts/SceneTransitionManager.cs
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -7,7 +7,15 @@ public class SceneTransitionManager : MonoBehaviour
 {
     public void LoadTo(string sceneName)
     {
+        FadeIOManager.instance.FadeOutToIn(() => Load(sceneName));
+
+    }
+
+    void Load(string sceneName)
+    {
+
         SoundManager.instance.PlayBGM(sceneName);
         SceneManager.LoadScene(sceneName);
     }
+
 }
