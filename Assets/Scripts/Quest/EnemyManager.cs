e7924a8fa4d85bb8a1579c5125f97e4b4936fef3
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-25 13:44:34 +0900

    ヒットエフェクトの実装

--- Assets/Scripts/Quest/EnemyManager.cs
+++ Assets/Scripts/Quest/EnemyManager.cs
@@ -10,6 +10,7 @@ public class EnemyManager : MonoBehaviour
     public new string name;
     public int hp;
     public int at;
+    public GameObject hitEffect;
 
     public void Attack(PlayerManager player)
     {
@@ -18,6 +19,7 @@ public class EnemyManager : MonoBehaviour
 
     public void Damage(int damage)
     {
+        Instantiate(hitEffect, this.transform);
         transform.DOShakePosition(0.3f, 0.5f, 20, 0, false, true);
         hp -= damage;
         if (hp <= 0)
